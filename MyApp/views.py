2911a97278fc4973947ffe9676c99f546380ef6c
Shrivas <rishabh.shrivas@arcadis.com>
2019-11-27 11:10:11 +0530

    Create Simple API using Python Django REST Framework

---
+++ MyApp/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from django.http import JsonResponse
+from django.core import serializers
+from django.conf import settings
+import json
+import math
+# Create your views here.
+# Auther: Rishabh Shrivas
+
+@api_view(["POST"])
+def IdealWeight(heightdata):
+    try:
+        height=json.loads(heightdata.body)
+        weight=str((height**2)*23)
+
+        return JsonResponse("Ideal weight should be:" + weight + " kg", safe=False)
+    except ValueError as e:
+        return Response(e.args[0],status.HTTP_400_BAD_REQUEST)
+
+
